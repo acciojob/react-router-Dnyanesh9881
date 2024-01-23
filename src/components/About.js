72fe3be793cc563dfc4a0566778e6ef988f4a8e5
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-01-23 04:37:58 +0000

    add

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const About=()=>{
+
+    return (
+        <div>
+            <p>This is sample react router program</p>
+        </div>
+    )
+}
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 
 import React from "react";
 import './../styles/App.css';
+import {Routes, Route} from "react-router";
+import Home from "./Home";
+import About from "./About";
+import { useNavigate } from "react-router-dom";
 
 const App = () => {
+  let navigate=useNavigate();
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <div>
+          <div>
+            <ul>
+              <li onClick={()=>navigate("/")}>Home</li>
+              <li onClick={()=>navigate("/about")}>About</li>
+            </ul>
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+
+          </Routes>
+        </div>
     </div>
   )
 }
